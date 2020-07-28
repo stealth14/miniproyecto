d1e912a482e3e40b3478f70348ae4ad306371254
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-07-28 14:04:30 -0500

    layout

---
+++ index.js
@@ -0,0 +1,32 @@
+function respuestavalida(){
+    var valida;
+
+    //procesar pregunta
+
+    return valida
+}
+
+function confirmar(){
+
+    if(respuestavalida()){
+
+        //chequear si es al ultima
+        //animar muñeco
+        //avanzar siguiente
+    }
+    else{
+        mostrarPenitencia();
+    }
+
+}
+
+
+function animarPersonaje(){
+
+}
+
+function mostrarPenitencia(){
+    $('#penitencia').append('haga lartijas')
+}
+
+mostrarPenitencia();
