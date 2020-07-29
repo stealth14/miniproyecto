17d8cccd9e5857a1b5dcdc214bb72979a409a94f
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-07-29 17:07:06 -0500

    animarAvatar mostrarPenitencia

--- index.js
+++ index.js
@@ -1,68 +1,86 @@
 //chente
-$( document ).ready( function() {
-  function respuestavalida() {
+function respuestavalida() {
     var valida;
 
     //procesar pregunta
 
     return valida
-  }
+}
 
-  function confirmar() {
+function confirmar() {
 
-    if( respuestavalida() ) {
+    if (respuestavalida()) {
 
-      //chequear si es al ultima
-      //animar muñeco
-      //avanzar siguiente
+        animarAvatar();
     } else {
-      mostrarPenitencia();
+        mostrarPenitencia();
     }
 
-  }
+}
 
 //ronny
-  function animarPersonaje() {
+const animarAvatar = () => {
 
-  }
+	$( "#avatar" ).animate({
+        left: `+=100`,
+      }, 1000, function() {
+        // Animation complete.
+        console.log( "animated!" );
+      });
+
+}
+
+
 
 //diego
 
+function mostrarPenitencia() {
 
-  let numberPenitencia;
-  $( '#verificarRespuesta' ).on( 'click', function() {
-    mostrarPenitencia();
-
-  } )
-})
-  //}
-  function mostrarPenitencia() {
-    $( '#penitencia' ).html( '' );
-    $( '.opsy' ).toggleClass( 'opsy opsy-mostrar' );
-
-    numberPenitencia = Math.floor( Math.random() * (21 - 1) ) + 1;
+    numberPenitencia = Math.floor(Math.random() * (21 - 1)) + 1;
     const penitencias = [
-      'Realize 5 sentadillas',
-      'Realize 10 saltos de tijeras',
-      'Realize 5 lagartijas',
-      'Realize 6 sapitos',
-      'Realize 15 saltos de tijeras',
-      'realize 8 abdominales',
-      'Realize 20 segundos de posicion plancha',
-      'Realize 15 segundos de posicion plancha',
-      'Realize 15 segundos de posicion plancha lateral',
-      'realize 10 sentadillas',
-      'realize 10 lagartijas',
-      'realize 2 minutos de estiramiento',
-      'realize 3 minutos de estiramient',
-      'realize 8 lagartijas',
-      'realize 2 minutos de estiramiento',
-      'Realize 4 sapitos',
-      'mantenga la posición vaca/gato por 1 minuto',
-      'realize 5 flexiones de pecho',
-      'realize 5 flexiones de triceps',
-      "mantenga la posición plancha por 10 minuto"
+        'Realize 5 sentadillas',
+        'Realize 10 saltos de tijeras',
+        'Realize 5 lagartijas',
+        'Realize 6 sapitos',
+        'Realize 15 saltos de tijeras',
+        'realize 8 abdominales',
+        'Realize 20 segundos de posicion plancha',
+        'Realize 15 segundos de posicion plancha',
+        'Realize 15 segundos de posicion plancha lateral',
+        'realize 10 sentadillas',
+        'realize 10 lagartijas',
+        'realize 2 minutos de estiramiento',
+        'realize 3 minutos de estiramient',
+        'realize 8 lagartijas',
+        'realize 2 minutos de estiramiento',
+        'Realize 4 sapitos',
+        'mantenga la posición vaca/gato por 1 minuto',
+        'realize 5 flexiones de pecho',
+        'realize 5 flexiones de triceps',
+        "mantenga la posición plancha por 10 minuto"
     ]
-    $( '#penitencia' ).append( penitencias[numberPenitencia] );
-    $('.ventana-penitencia').css( 'background-color', '#e7d4a8' );
-  }
+
+    console.log(penitencias[numberPenitencia]);
+    $('.opsy').html('');
+    $('.opsy').append(penitencias[numberPenitencia]);
+
+}
+
+
+let numberPenitencia;
+$('#mostrarPenitencia').on('click', function () {
+    mostrarPenitencia();
+});
+
+  //}
+
+  $(document).ready(
+      ()=>{
+            $('#btn-animar').on(
+                'click',animarAvatar
+            )
+            console.log( "ready!" );
+            mostrarPenitencia();
+
+      }
+  )
